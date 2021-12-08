5663edb4136996715e4c778acab0b4b5656232f4
mikeyjk <michael.kiernan@healthengine.com.au>
2021-12-08 21:23:50 +0800

    finally passing against ze sample input

--- 2021/day4/ts/day4.ts
+++ 2021/day4/ts/day4.ts
@@ -9,19 +9,118 @@ const bingoNumbers = input
   .split(",")
   .map((stringNumber) => parseInt(stringNumber));
 
+type BoardCell = {
+  value: number;
+  isBingo: boolean;
+};
+
+type BingoRow = BoardCell[];
+type BingoColumn = BoardCell[];
+
+// store each column and row as contigous arrays
+type BingoBoard = {
+  columns: BingoColumn[];
+  rows: BingoRow[];
+};
+
 // read board numbers into array without hardcoding 5x5
 let boardCount = -1;
-const boardNumbers = input
+
+const bingoBoards = input
   .map((line) => line.split(" ").filter(Boolean))
-  .reduce<number[][]>((prev, current, index) => {
+  .reduce<BingoBoard[]>((prev, current, index) => {
     if (current.length === 0) {
       boardCount++;
     } else {
       prev = prev || [];
-      prev[boardCount] = prev[boardCount] || [];
-      prev[boardCount].push(...current.map((c) => parseInt(c)));
+      prev[boardCount] = prev[boardCount] || { columns: [], rows: [] };
+      prev[boardCount].rows = prev[boardCount].rows || [];
+      prev[boardCount].columns = prev[boardCount].columns || [];
+
+      // seems problematic to calculate isBingo ahead of time
+      prev[boardCount].rows.push(
+        current.map((cellChar, cellIndex) => {
+          const cellDigit = parseInt(cellChar);
+          prev[boardCount].columns[cellIndex] =
+            prev[boardCount].columns[cellIndex] || [];
+
+          prev[boardCount].columns[cellIndex][index] = prev[boardCount].columns[
+            cellIndex
+          ][index] || { value: 0, isBingo: false };
+
+          prev[boardCount].columns[cellIndex][index].value = cellDigit;
+          return { value: cellDigit, isBingo: false };
+        })
+      );
     }
     return prev;
   }, []);
 
-console.log({ input, bingoNumbers, boardNumbers });
+const hasNumberTriggeredBingo = (
+  bingoNumber: number,
+  sequence: BingoRow | BingoColumn
+): boolean => {
+  const requiredMatches = sequence.length;
+  let matches = 0;
+  sequence.forEach((boardCell) => {
+    if (boardCell.isBingo || bingoNumber === boardCell.value) {
+      matches++;
+      boardCell.isBingo = true;
+    }
+  });
+  return matches === requiredMatches;
+};
+
+let bingo = false;
+let winningNumber: number;
+let bingoSequence: BingoRow | BingoColumn;
+let winningBoard: BingoBoard;
+
+// I can 't think of a clever way of doing this, so just gonna brute force
+bingoNumbers.forEach((bingoNumber) => {
+  bingoBoards.forEach((bingoBoard) => {
+    bingoBoard.columns.forEach((bingoColumn) => {
+      if (!bingo && hasNumberTriggeredBingo(bingoNumber, bingoColumn)) {
+        bingoSequence = bingoColumn;
+        winningNumber = bingoNumber;
+        winningBoard = bingoBoard;
+        bingo = true;
+      }
+    });
+
+    bingoBoard.rows.forEach((bingoRow) => {
+      if (!bingo && hasNumberTriggeredBingo(bingoNumber, bingoRow)) {
+        bingoSequence = bingoRow;
+        winningNumber = bingoNumber;
+        winningBoard = bingoBoard;
+        bingo = true;
+      }
+    });
+  });
+});
+
+// I have an empty array in here, why D:
+const unmarkedNumbers: number[] = winningBoard.rows.reduce<number[]>(
+  (prevRow, currRow) => {
+    prevRow.push(
+      ...currRow
+        .filter((bingoCell) => !bingoCell.isBingo)
+        .map((unmarkedCell) => unmarkedCell.value)
+    );
+
+    return prevRow;
+  },
+  []
+);
+
+const unmarkedNumberSum = unmarkedNumbers.reduce((prevCell, currCell) => {
+  return (prevCell += currCell);
+}, 0);
+
+console.log({
+  winningNumber,
+  bingoSequence: bingoSequence.map((sequence) => sequence.value),
+  unmarkedNumbers,
+  unmarkedNumberSum,
+  score: winningNumber * unmarkedNumberSum,
+});
